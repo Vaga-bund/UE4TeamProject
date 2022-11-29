ca2fd9c37a6b0b332a5ece9aebb4055bfca31cc5
prg217 <prg217@naver.com>
2022-11-29 13:21:38 +0900

    공격 구현중
    플레이어 캐릭터 추가, 플레이어 게임 인스턴스 서브클래스 추가

---
+++ Source/UE4Project/PlayerCharacter.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+//박연지
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerGameInstanceSubsystem.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class UE4PROJECT_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+	//플레이어 회전도 하게 해야함
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void Attack(int32 monsterPower);
+
+private:
+	UGameInstanceSubsystem* playerGISS;
+};
---
+++ Source/UE4Project/PlayerGameInstanceSubsystem.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+//박연지
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "PlayerGameInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4PROJECT_API UPlayerGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	//플레이어 HP 및 플레이어 공격력은 게임이 끝날때까지 유지 되기 때문에 게임 인스턴스로 관리함
+	UPROPERTY()
+	int32 playerHP = 100;
+
+	UPROPERTY()
+	int32 playerPower = 100;
+};
